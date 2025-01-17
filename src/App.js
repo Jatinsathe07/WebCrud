ef585901a97909d2f3b34efa8487552290ac4768
Jatinsathe07 <127086799+Jatinsathe07@users.noreply.github.com>
2025-01-17 21:27:14 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Home from './Home';
+import Create from './Create';
+import Update from './Update';
+import Read from './Read';
+
+function App() {
+  return (
+    
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/create' element={<Create></Create>}></Route>
+      <Route path='/update/:id' element={<Update></Update>}></Route>
+      <Route path='/read/:id' element={<Read></Read>}></Route>
+    </Routes>
+   </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Create.jsx
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import {  Link, useNavigate } from 'react-router-dom'
+
+function Create() {
+
+  const [values, setValues] =useState({
+    name:'',
+    email:'',
+    department:''
+  })
+
+  const navigate = useNavigate();
+
+
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    axios.post('http://localhost:3000/users', values)
+    .then(res=>{
+      console.log(res);
+      navigate('/')
+    })
+    .catch(err => console.log(err))
+  }
+
+
+
+  return (
+    <div className='d-flex w-100 vh-100 justify-content-center align-items-center bg-light'>
+      <div className='w-50 border bg-white shadow px-5 pt-3 pb-5 rounded'>
+        <h1>Add a user</h1>
+        <form onSubmit={handleSubmit}>
+          <div className='mb-2'>
+            <label htmlFor="name">Full Name</label>
+            <input type="text"  name='name' className='form-control' placeholder='Enter Name'
+            onChange={e => setValues({...values, name: e.target.value})}/>
+          </div>
+
+          <div className='mb-2'>
+            <label htmlFor="name">Email</label>
+            <input type="email"  name='email' className='form-control' placeholder='Enter Email'
+            onChange={e => setValues({...values, email: e.target.value})}/>
+          </div>
+
+          <div className='mb-2'>
+            <label htmlFor="name">Department</label>
+            <input type="text"  name='department' className='form-control' placeholder='Enter Department'
+            onChange={e => setValues({...values, department: e.target.value})}/>
+          </div>
+          <button className='btn btn-success'>Submit</button>
+          <Link to='/' className='btn btn-primary ms-3'>Back</Link>
+        </form>
+      </div>
+      
+
+
+    </div>
+  )
+}
+
+export default Create
